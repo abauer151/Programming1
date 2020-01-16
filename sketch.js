18d60123cf632f2a0dbb9a1e65236cd9d6c28a6c
abauer151 <55888695+abauer151@users.noreply.github.com>
2020-01-16 12:28:06 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,17 +1,36 @@
+var medkit;
+var rapidfire;
+var bullet;
+var spaceship;
+var mx = -100; //medkit x
+var my = 0; //medkit y
+var rx = 0; //rapidfire x
+var ry = 0; //rapidfire y
+var bx = -100; //bullet x
+var by = 600; //bullet y
+var sx = 0; //spaceship x
+var sy = 0; //spaceship y 
+var bulletTrackerRad = 1;
+var SpaceShipSpeed = 10;
+//aidans code
 var meteor =[meteor, meteor, meteor, meteor, meteor, meteor];
+var vert = 0;//vertical 
+var horiz = 0;
 var meteorHeight = 150;
 var meteorWidth = 150;
-var wallBuffer = 300;
-let rad = 5; // Width of the shape
-var buffer = 50;//used to set meteor and tracker at same starting position
+var wallBuffer = 300;//distance from the canvas that the meteors bounce off of
+let rad = 50; // Width of the shape
+var buffer = 80;//used to set meteor and tracker at same starting position
+var bulletXBuffer = 13;//used to set the bullet and the tracker at the same position
+var bulletYBuffer = 30;
 let xpos1, ypos1; // Starting position of shape (used for first meteor)
 let xpos2, ypos2;//used for 2nd meteor
 let xpos3, ypos3;//used for 3rd meteor
 let xpos4, ypos4;//used for 4th meteor
 let xpos5, ypos5;//used for 5th meteor
 let xpos6, ypos6;//used for 6th meteor
-let xspeed = 10; // Speed of the shape
-let yspeed = 10; // Speed of the shape
+let xspeed = 5; // Speed of the shape
+let yspeed = 5; // Speed of the shape
 
 var xdirection1 = Math.random();//changes bounce direction
 var ydirection1 = Math.random();
@@ -26,19 +45,24 @@ var ydirection5 = Math.random();
 var xdirection6 = Math.random();
 var ydirection6 = Math.random();
 
-function start(){
-	preload();
-	setup();
-	draw();
-	
-}
-
 function preload(){
-	meteor = loadImage('big meteor.png');
+	// load image
+	medkit = loadImage('/health.png');
+	rapidfire = loadImage('/rapidFire.png');
+	bullet = loadImage('/bullet.png');
+	spaceship = loadImage('/Newspaceship.png');
+	//aidans code
+	meteor = loadImage('/Big Meteor.png');
 }
+ 
 function setup(){
-	createCanvas(1000,800);
+	// set canvas size
+	createCanvas(800,700);
 	background('black');
+	rx = width/2;
+	sx = width/2;
+	sy = height/2;
+	//aidans code 
 	noStroke();
 	frameRate(30);
 	ellipseMode(RADIUS);
@@ -56,10 +80,49 @@ function setup(){
 	xpos6 = 900;
 	ypos6 = 900;
 }
-
+ 
 function draw(){
-	background(0);//makes background black
+	// Draws power ups and spaceship
+	background(0);
+	image(medkit, mx, my);
+	image(rapidfire, rx, ry);
+	image(bullet, bx, by);
+	image(spaceship, sx, sy);
+	move();
+	//moves spaceship with w,a,s,d
+	if (keyIsDown(87)){ //w
+		sy = sy - SpaceShipSpeed;
+	}
 	
+	if (keyIsDown(83)){ //s
+		sy = sy + SpaceShipSpeed;
+	}
+	
+	if (keyIsDown(65)){ //a
+		sx = sx - SpaceShipSpeed;
+	}
+	
+	if (keyIsDown(68)){ //d
+		sx = sx + SpaceShipSpeed;
+	}
+	//moves spaceship with arrows
+	if (keyIsDown(38)){ //up
+		sy = sy - SpaceShipSpeed;
+	}
+	
+	if (keyIsDown(40)){ //down
+		sy = sy + SpaceShipSpeed;
+	}
+	
+	if (keyIsDown(37)){ //left
+		sx = sx - SpaceShipSpeed;
+	}
+	
+	if (keyIsDown(39)){ //right
+		sx = sx + SpaceShipSpeed;
+	}
+	
+	//aidans code 
 	//wall code for first meteor
 	xpos1 = xpos1 + xspeed * xdirection1;
 	ypos1 = ypos1 + yspeed * ydirection1;
@@ -122,6 +185,7 @@ function draw(){
 
   // Draw the trackers and meteor images
 	var tracker = [ellipse(xpos1, ypos1, rad, rad),ellipse(xpos2, ypos2, rad, rad),ellipse(xpos3, ypos3, rad, rad),ellipse(xpos4, ypos4, rad, rad),ellipse(xpos5, ypos5, rad, rad),ellipse(xpos6, ypos6, rad, rad)];
+	var bulletTracker = ellipse(bx+bulletXBuffer, by+bulletYBuffer,bulletTrackerRad, bulletTrackerRad)
 	image(meteor, xpos1-buffer, ypos1-buffer, meteorWidth, meteorHeight );
 	//top left meteor
 	image(meteor, xpos2-buffer, ypos2-buffer, meteorWidth, meteorHeight );
@@ -134,7 +198,75 @@ function draw(){
 	//bottom center meteor
 	image(meteor, xpos6-buffer, ypos6-buffer, meteorWidth, meteorHeight );
 	//bottom right meteor
+	move();
 	
+}
+
+
+function move(){
+	// Moves power ups and bullet
+	mx = mx + 2;
+	my = my + 0;
+	rx = rx + 2;
+	ry = ry + 0;
+	bx = bx + 0;
+	by = by - 10;
 	
+	//changes medkit x when it gets off screen
+	if(mx > width+100){
+		mx = -100;
+	}
+	
+	//changes rapidfire x when it gets off screen
+	if(rx > width+100){
+		rx = -100;
+	}
+	
+	//changes bullet x and y when it gets off screen
+	if(by < -50){
+		by = 600;
+		bx = -100;
+	}
+	
+	//stops spaceship from going off screen
+	if(sy < 0){
+		sy = 0;
+		sx = sx;
+	}
+	
+	if(sy > height-30){
+		sy = height-30;
+		sx = sx;
+	}
+	
+	if(sx < 0){
+		sy = sy;
+		sx = 0;
+	}
+	
+	if(sx > width-30){
+		sy = sy;
+		sx = width-30;
+	}
+	
+	//when bullet hits power up, move power up x and y
+	if(by === my && bx === mx){
+		mx = -100;
+	}
+}
+
+function keyPressed(){
+	//shoot bullet
+	if (keyCode === 32){
+		by = sy;
+		bx = sx;
+	}
+}
+
+function start(){
+	preload();
+	setup();
+	draw();
+	keyPressed();
 }
 start();
